877c5d94629312c4d0f4c11f9c53e738ed47a02c
longvb <vuongbaolong48@gmail.com>
2021-12-17 15:30:49 +0700

    update create_input_file

--- create_input_files.py
+++ create_input_files.py
@@ -2,10 +2,10 @@ from utils import create_input_files
 
 if __name__ == '__main__':
     # Create input files (along with word map)
-    create_input_files(dataset='coco',
-                       karpathy_json_path='../caption data/dataset_coco.json',
-                       image_folder='/media/ssd/caption data/',
+    create_input_files(dataset='flickr30k',
+                       karpathy_json_path='/content/drive/MyDrive/lame_caption_recommendation/data_json/dataset_flickr30k.json',
+                       image_folder='/content/drive/MyDrive/lame_caption_recommendation/flickr30k_images/flickr30k_images',
                        captions_per_image=5,
                        min_word_freq=5,
-                       output_folder='/media/ssd/caption data/',
+                       output_folder='/content/drive/MyDrive/lame_caption_recommendation/out',
                        max_len=50)
