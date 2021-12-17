07d6cbda133b00fcdc3140f63b3ccb4e1892c413
longvb <vuongbaolong48@gmail.com>
2021-12-17 16:41:41 +0700

    update path train.py

--- train.py
+++ train.py
@@ -11,8 +11,8 @@ from utils import *
 from nltk.translate.bleu_score import corpus_bleu
 
 # Data parameters
-data_folder = '/media/ssd/caption data'  # folder with data files saved by create_input_files.py
-data_name = 'coco_5_cap_per_img_5_min_word_freq'  # base name shared by data files
+data_folder = '/content/drive/MyDrive/lame_caption_recommendation/out'  # folder with data files saved by create_input_files.py
+data_name = 'flickr30k_5_cap_per_img_5_min_word_freq'  # base name shared by data files
 
 # Model parameters
 emb_dim = 512  # dimension of word embeddings
