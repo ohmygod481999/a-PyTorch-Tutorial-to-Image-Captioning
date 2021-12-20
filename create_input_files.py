78325ae760413956898aef241229c530f532203a
longvb <vuongbaolong48@gmail.com>
2021-12-21 01:20:50 +0700

    update

--- create_input_files.py
+++ create_input_files.py
@@ -2,10 +2,10 @@ from utils import create_input_files
 
 if __name__ == '__main__':
     # Create input files (along with word map)
-    create_input_files(dataset='flickr30k',
-                       karpathy_json_path='/content/dataset_flickr30k.json',
-                       image_folder='/content/flickr30k_images/flickr30k_images',
+    create_input_files(dataset='flickr8k',
+                       karpathy_json_path='/content/drive/MyDrive/lame_caption_recommendation/flickr8k_translate/vietnamese_flickr8k.json',
+                       image_folder='/content/drive/MyDrive/lame_caption_recommendation/flickr8k_images',
                        captions_per_image=5,
                        min_word_freq=5,
-                       output_folder='/content/out',
+                       output_folder='/content/drive/MyDrive/lame_caption_recommendation/out_flickr8k',
                        max_len=50)
