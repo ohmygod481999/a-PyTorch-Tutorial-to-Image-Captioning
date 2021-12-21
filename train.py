e826b46fcc0b3e095d55065af5b1b756d7b8805a
longvb <vuongbaolong48@gmail.com>
2021-12-21 10:18:19 +0700

    update

--- train.py
+++ train.py
@@ -35,7 +35,7 @@ alpha_c = 1.  # regularization parameter for 'doubly stochastic attention', as i
 best_bleu4 = 0.  # BLEU-4 score right now
 print_freq = 100  # print training/validation stats every __ batches
 fine_tune_encoder = False  # fine-tune encoder?
-checkpoint = "/content/drive/MyDrive/lame_caption_recommendation/files for A PyTorch Tutorial For Image Captioning/BEST_checkpoint_coco_5_cap_per_img_5_min_word_freq.pth.tar"  # path to checkpoint, None if none
+checkpoint = None # "/content/drive/MyDrive/lame_caption_recommendation/files for A PyTorch Tutorial For Image Captioning/BEST_checkpoint_coco_5_cap_per_img_5_min_word_freq.pth.tar"  # path to checkpoint, None if none
 
 
 def main():
